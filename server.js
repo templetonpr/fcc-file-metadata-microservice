3e390648efba82026f44dde28a5273d59ec5ac82
Patrick Templeton <contact@patricktempleton.xyz>
2016-04-24 14:23:23 -0400

    add multer functionality

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 "use strict";
 
-let fs = require('fs');
 let multer = require('multer');
 let app = require('express')();
 
-app.get('/', function(req,res) {
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname + '/uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + '.' + file.originalname.split(".")[1])
+  }
+});
+
+let upload = multer({ storage: storage });
+
+app.get('/', (req, res) => {
   // instructions page
   res.sendFile(__dirname + '/public/index.html');
+})
+
+.post('/', upload.single('uploaded-file'), (req, res) => {
+  console.log(req.file);
+  res.json({
+    "Name": req.file.originalname,
+    "Size": req.file.size
+  });
 });
 
 let port = process.env.PORT || 8080;
