1746a9f4c373300cca1b2d231b04aad0e0dbda8f
Patrick Templeton <contact@patricktempleton.xyz>
2016-04-25 19:18:50 -0400

    Add max upload size and env var options

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# fcc-file-metadata-microservice
+# FCC File Metadata Microservice
 Microservice to accept a file and return JSON string with the file size
+
+### Usage
+
+`npm install` to install dependencies.
+
+`npm start` to start server.
+
+Uploading a file returns a JSON string containing the file name and size in bytes.
+
+#### Environment variables
+
+`MAX_FILE_SIZE` to set (you guessed it) max file size in bytes. Default is 1MB if not set.
+Note: There is some text under the upload form that says max upload size is 1MB which you might consider changing also.
+
+`SAVE_FILES` - Set this to save files to /uploads. Otherwise, default behavior is to save to memory (and discard after returning file info)
+
+`PORT` to set port. Default is 8080.
--- server.js
+++ server.js
@@ -3,16 +3,31 @@
 let multer = require('multer');
 let app = require('express')();
 
-let storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, __dirname + '/uploads');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.mimetype.split("/")[0] + '-' + Date.now() + '.' + file.mimetype.split("/")[1])
-  }
-});
+// maximum allowed file size
+const MAX_FILE_SIZE = process.env.MAX_FILE_SIZE || 1000000;
 
-let upload = multer({ storage: storage });
+// If SAVE_FILES is set, store in /uploads, else use memory
+let storage;
+if (process.env.SAVE_FILES) {
+  console.log("diskStorage is set");
+  storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, __dirname + '/uploads');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.mimetype.split("/")[0] + '-' + Date.now() + '.' + file.mimetype.split("/")[1])
+    }
+  });
+} else {
+  console.log("diskStorage is not set");
+  storage = multer.memoryStorage();
+}
+
+// upload settings
+let upload = multer({
+  storage: storage,
+  limits: {fileSize: MAX_FILE_SIZE}
+}).single('uploaded-file');
 
 app.get('/', (req, res) => {
   // instructions page
@@ -20,15 +35,19 @@ app.get('/', (req, res) => {
 })
 
 .get('/*', (req, res) => {
-  // redirect everything to /
+  // redirect everything else to /
   res.redirect('/');
 })
 
 .post('/', (req, res) => {
-  multer().single('uploaded-file')(req, res, (err) => {
+  upload(req, res, (err) => {
     if (err) {
       console.log(err);
-      res.send("<h1>There was an error processing your file. Please try again.</h1>");
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        res.status(413).send("<p>Error: upload exceeded max size of " + MAX_FILE_SIZE + " bytes.");
+      } else {
+        res.send("<p>There was an error processing your file. Please try again.</p>");
+      }
     } else {
       console.log(req.file);
       res.json({
